e037150dc2fe5f54cc87afa48dd8266366cdf61b
lmayorga <lmayorga@lm3corp.com>
2019-09-23 22:07:10 -0400

    increate instance type and update output variables

--- my-variables.tfvars
+++ my-variables.tfvars
@@ -1,7 +1,13 @@
 region = "us-east-1"
+
 key_name = "lm3corp"
-region_list  = ["us-east-1a","us-east-1b"]
-instance_type = "t2.medium"
+
+region_list = ["us-east-1a", "us-east-1b"]
+
+instance_type = "t2.xlarge"
+
 instance_client_ips = ["10.0.1.20", "10.0.1.21"]
+
 instance_server_ips = "10.0.1.50"
+
 nomad_version = "0.9.5"
--- output.tf
+++ output.tf
@@ -8,9 +8,9 @@ output "nomad_server_public_dns" {
 }
 
 output "nomad_client_ips"{
-  value = ["${aws_instance.nomad_client.*.public_ip}"]
+  value = "${aws_instance.nomad_client.*.public_ip}"
 }
 
 output "nomad_client_public_dns"{
-  value = ["${aws_instance.nomad_client.*.public_dns}"]
+  value = "${aws_instance.nomad_client.*.public_dns}"
 }
